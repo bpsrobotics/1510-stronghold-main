988ae22c494471a7b53a7d58664df02a697a1be3
annanixon <annapnixon@gmail.com>
2016-02-06 15:39:35 -0800

    Edited BallCollector subsystem

--- src/org/usfirst/frc/team1510/robot/subsystems/BallCollector.java
+++ src/org/usfirst/frc/team1510/robot/subsystems/BallCollector.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team1510.robot.subsystems;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.Relay;
-import edu.wpi.first.wpilibj.Encoder;
+
 
 
 /**
@@ -14,13 +14,23 @@ public class BallCollector extends Subsystem {
 	// Arm systems
     private Talon armMotor = new Talon(1);
     private Relay rollerMotor = new Relay(2 , Relay.Direction.kForward);
+    private EncoderSubsystem armEncoder  = new EncoderSubsystem(1,2);
     
     
-    public void armLower() { 
-    	
+    public void armLower(int distance) { 
+    	armEncoder.reset();
+    	armEncoder.reverse();
+    	while(armEncoder.getDistance() < distance)
+    	{
+    		armMotor.set(.5);
+    	}
     }
-    public void armRaise() { 
-    	
+    public void armRaise(int distance) { 
+    	armEncoder.reset();
+    	while(armEncoder.getDistance() < distance)
+    	{
+    		armMotor.set(.5);
+    	}
     }
     public void on(){
 		rollerMotor.set(Relay.Value.kForward);
