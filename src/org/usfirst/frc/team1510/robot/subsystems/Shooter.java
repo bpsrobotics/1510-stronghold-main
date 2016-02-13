a2b5455561cbab8311768c4699cca29db1e11e44
Alex <Alex@172.23.82.28>
2016-02-12 16:17:33 -0800

    Updated Shooter Subsystem.

--- src/org/usfirst/frc/team1510/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team1510/robot/subsystems/Shooter.java
@@ -9,36 +9,20 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Shooter extends Subsystem {
 	private Talon shooterMotor = new Talon(1);
-	private EncoderSubsystem shooterEncoder  = new EncoderSubsystem(1,2);
-	 public void armLower(int distance) { 
-	    	shooterEncoder.reset();
-	    	shooterEncoder.reverse();
-	    	while(shooterEncoder.getDistance() < distance)
-	    	{
-	    		shooterMotor.set(.5);
+	private boolean justShot=false;
+	 public void Fire() { 
+		 shooterMotor.set(.5);
+		 justShot=true;
+	    	
+	    	if(justShot==true){
+	    		shooterMotor.stopMotor();
+	    		justShot=false;
 	    	}
+	 }
+	 public void initDefaultCommand() {
+	        // Set the default command for a subsystem here.
+	        //setDefaultCommand(new MySpecialCommand());
 	    }
-	    public void armRaise(int distance) { 
-	    	shooterEncoder.reset();
-	    	while(shooterEncoder.getDistance() < distance)
-	    	{
-	    		shooterMotor.set(.5);
-	    		
-	    	}
-	    }
-	    public void on(){
-	    	shooterMotor.set(Relay.Value.kForward);
-	    	shooterMotor.set(Relay.Value.kOn);
-	    }
-	    public void off() {
-	    	shooterMotor.set(Relay.Value.kOff);
-	    } 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
 }
+	 
 
