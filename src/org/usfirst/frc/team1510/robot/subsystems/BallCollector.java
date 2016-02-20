719fc95f39b9ad7301d1c3f075ca2222059c5d29
annanixon <annapnixon@gmail.com>
2016-02-20 08:52:52 -0800

    Replaced Encoders with limit switches

--- src/org/usfirst/frc/team1510/robot/subsystems/BallCollector.java
+++ src/org/usfirst/frc/team1510/robot/subsystems/BallCollector.java
@@ -4,7 +4,8 @@ import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.Encoder;
-
+import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 /**
  *
@@ -14,24 +15,26 @@ public class BallCollector extends Subsystem {
 	// Arm systems
     private Talon armMotor = new Talon(1);
     private Relay rollerMotor = new Relay(2 , Relay.Direction.kForward);
-    private Encoder armEncoder  = new Encoder(7,8);
+    private DigitalInput limitSwitch = new DigitalInput(1);
+    private Counter counter = new Counter(limitSwitch);
     
-    
-    public void armLower(int distance) { 
-    	armEncoder.reset();
-    	armEncoder.setReverseDirection(true);
-    	while(armEncoder.getDistance() < distance)
-    	{
-    		armMotor.set(0.25);
-    	}
+    public boolean isSwitchSet() {
+        return counter.get() > 0;
     }
-    public void armRaise(int distance) { 
-    	armEncoder.reset();
-	armEncoder.setReverseDirection(false);
-    	while(armEncoder.getDistance() < distance)
-    	{
-    		armMotor.set(-0.25);
-    	}
+    
+    public void initializeCounter() {
+        counter.reset();
+    }
+    
+    public void extend(int distance) { 
+    	initializeCounter();
+    	armMotor.set(-.25);
+    	if(isSwitchSet()) armMotor.set(0);
+    }
+    public void retract(int distance) { 
+    	initializeCounter();
+    	armMotor.set(.25);
+    	if(isSwitchSet()) armMotor.set(0);
     }
     public void on(){
 	rollerMotor.set(Relay.Value.kForward);
--- src/org/usfirst/frc/team1510/robot/subsystems/WheelArms.java
+++ src/org/usfirst/frc/team1510/robot/subsystems/WheelArms.java
@@ -3,6 +3,8 @@ package org.usfirst.frc.team1510.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Encoder;
 
@@ -13,7 +15,6 @@ public class WheelArms extends Subsystem {
     
 	// Arm systems
     private Talon armMotor = new Talon(0);
-    private EncoderSubsystem armEncoder = new EncoderSubsystem(1,2);
     // Wheel motors
     private CANTalon wheelMotor = new CANTalon(6);
     
@@ -24,6 +25,28 @@ public class WheelArms extends Subsystem {
     private double goalSpeed = 0;
     private double currentSpeed = 0;
     private double speedAdjustPerCycle = 0.04;
+    
+    private DigitalInput limitSwitch = new DigitalInput(1);
+    private Counter counter = new Counter(limitSwitch);
+    
+    public boolean isSwitchSet() {
+        return counter.get() > 0;
+    }
+    
+    public void initializeCounter() {
+        counter.reset();
+    }
+    
+    public void extend(int distance) { 
+    	initializeCounter();
+    	armMotor.set(-.25);
+    	if(isSwitchSet()) armMotor.set(0);
+    }
+    public void retract(int distance) { 
+    	initializeCounter();
+    	armMotor.set(.25);
+    	if(isSwitchSet()) armMotor.set(0);
+    }
     
     public void move(double speed) {
     	if (!enabled) return;    	
@@ -43,14 +66,6 @@ public class WheelArms extends Subsystem {
      	
     	// Update motor throttle
     	wheelMotor.set(currentSpeed);
-    }
-    
-    public void retract(){
-    	
-    }
-    
-    public void extend(){
-    	
     }
 	
     public void enable() {
