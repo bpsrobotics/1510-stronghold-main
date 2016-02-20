d2b34a6cc1ff9c02b9d3043ff7201b4ce0dfd630
Stephen Brimhall <stephen.brimhall@icloud.com>
2016-02-20 14:09:28 -0800

    Created Turn command

---
+++ src/org/usfirst/frc/team1510/robot/commands/Turn.java
@@ -0,0 +1,55 @@
+package org.usfirst.frc.team1510.robot.commands;
+
+import org.usfirst.frc.team1510.robot.Robot;
+import org.usfirst.frc.team1510.robot.subsystems.Drive;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.*;
+
+/**
+ *
+ */
+public class Turn extends Command {
+
+    // Drive code
+    private Drive drive = Robot.drive;
+
+    // Degrees to turn
+    private double degrees;
+
+    // Records if complete or not
+    private isComplete = false;
+    
+    public Move(double degrees) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.drive);
+
+	this.degrees = degrees;
+    }
+    
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	drive.enable();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+	isComplete = drive.turn(this.degrees,0.85);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return isComplete;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	drive.disable();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
