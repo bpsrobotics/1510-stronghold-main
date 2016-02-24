a862618beaacbc73af83285b9cca7204ddf3f79e
Penguimicus <AFBreiling05@bsd48.org>
2016-02-23 17:20:30 -0800

    Completed Scaling Motors (Uses Joystick Input)

--- src/org/usfirst/frc/team1510/robot/subsystems/ScalingMotor.java
+++ src/org/usfirst/frc/team1510/robot/subsystems/ScalingMotor.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team1510.robot.subsystems;
 
+import org.usfirst.frc.team1510.robot.Robot;
+
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -8,32 +11,34 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class ScalingMotor extends Subsystem {
-	private Talon scalingMotor = new Talon(0);
-	private boolean enabled = false;
-			private CANTalon climbMotor = new CANTalon(1);
-			 private double[] goalSpeed = {0,0};
-			    private double[] currentSpeed = {0,0};
-			    private double speedAdjustPerCycle = 0.04;
-			    
-			    public void move() {
-			    	if (!enabled) return;
-			
-			if(Math.abs(goalSpeed[0] - currentSpeed[0]) < speedAdjustPerCycle)
-			{
-	    		// If within one-cycle range of goal
-	    		currentSpeed[0] = goalSpeed[0];}
-	    	else if (currentSpeed[0] > goalSpeed[0])
-	    		// If more than goal
-	    		{currentSpeed[0] -= speedAdjustPerCycle;}
-	    	else if (currentSpeed[0] < goalSpeed[0])
-	    		// If less than goal
-	    		{currentSpeed[0] += speedAdjustPerCycle;
-			}
-			    }
-			    public void enable() {
-			    	
-			    }
+	private Talon scalingMotorLeft = new Talon(0);
+	private Talon scalingMotorRight = new Talon(0);
+	public final double MaxSpeed = 88.5;
+	private Encoder encoder1=new Encoder(7,8);
+	private Encoder encoder2=new Encoder(7,8);
 
+	public void ExtendMax()
+	{
+		scalingMotorLeft.set(MaxSpeed);
+		scalingMotorRight.set(MaxSpeed);
+	}
+	public void ExtendWithJoystick()
+	{
+		
+		double OICoords= Robot.oi.leftStick.getY();
+		if(OICoords>0)
+		{
+			scalingMotorLeft.set(MaxSpeed);
+			scalingMotorRight.set(MaxSpeed);
+		}
+		else if(OICoords<0)
+		{
+			scalingMotorLeft.set(-1*MaxSpeed);
+			scalingMotorRight.set(-1*MaxSpeed);
+		}
+		else{scalingMotorLeft.stopMotor();}
+	}
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
