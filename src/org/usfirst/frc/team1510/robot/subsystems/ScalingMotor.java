daac5b9740fc556597a80249eeaceb70901fcdce
Penguimicus <AFBreiling05@bsd48.org>
2016-02-23 18:05:49 -0800

    Created Deploy Scaling Command.

--- src/org/usfirst/frc/team1510/robot/subsystems/ScalingMotor.java
+++ src/org/usfirst/frc/team1510/robot/subsystems/ScalingMotor.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team1510.robot.subsystems;
 
 import org.usfirst.frc.team1510.robot.Robot;
-
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
@@ -11,18 +10,19 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class ScalingMotor extends Subsystem {
-	private Talon scalingMotorLeft = new Talon(0);
-	private Talon scalingMotorRight = new Talon(0);
-	public final double MaxSpeed = 88.5;
+	private static Talon scalingMotorLeft = new Talon(0);
+	private static Talon scalingMotorRight = new Talon(0);
+	public final static double MaxSpeed = 88.5;
 	private Encoder encoder1=new Encoder(7,8);
 	private Encoder encoder2=new Encoder(7,8);
+	public static boolean terminate=false;
 
 	public void ExtendMax()
 	{
 		scalingMotorLeft.set(MaxSpeed);
 		scalingMotorRight.set(MaxSpeed);
 	}
-	public void ExtendWithJoystick()
+	public static void ExtendWithJoystick()
 	{
 		
 		double OICoords= Robot.oi.leftStick.getY();
@@ -36,7 +36,15 @@ public class ScalingMotor extends Subsystem {
 			scalingMotorLeft.set(-1*MaxSpeed);
 			scalingMotorRight.set(-1*MaxSpeed);
 		}
-		else{scalingMotorLeft.stopMotor();}
+		else{
+		scalingMotorLeft.stopMotor();
+		scalingMotorLeft.stopMotor();
+		}
+	}
+	public static void terminate()
+	{
+		if(scalingMotorLeft.getSpeed()==0 && scalingMotorRight.getSpeed()==0)
+		{terminate=true;}
 	}
 	
     public void initDefaultCommand() {
