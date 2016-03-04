361799324d5e4a4a4051fe8354f6010c48ea8889
Stephen Brimhall <stephen.brimhall@icloud.com>
2016-03-03 16:48:35 -0800

    dammit nathan

--- src/org/usfirst/frc/team1510/robot/Robot.java
+++ src/org/usfirst/frc/team1510/robot/Robot.java
@@ -56,7 +56,8 @@ public class Robot extends IterativeRobot {
     	startingPosition.addObject("3", Autonomous.StartingPosition.POS3);
     	startingPosition.addObject("4", Autonomous.StartingPosition.POS4);
     	startingPosition.addObject("5", Autonomous.StartingPosition.POS5);
-    	
+
+	defense = new SendableChooser();
     	defense.addDefault("Portcullis", Autonomous.Defense.PORTCULLIS);
     	defense.addObject("Cheval de Frise", Autonomous.Defense.CHEVAL_DE_FRISE);
     	defense.addObject("Moat", Autonomous.Defense.MOAT);
