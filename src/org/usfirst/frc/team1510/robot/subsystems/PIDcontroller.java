3cce58668e9b0ae2528a2120a2734d7a5eb7c117
Solomon Greenberg <solomongreenberg@gmail.com>
2016-03-03 17:11:13 -0800

    finished pid i think

--- src/org/usfirst/frc/team1510/robot/subsystems/PIDcontroller.java
+++ src/org/usfirst/frc/team1510/robot/subsystems/PIDcontroller.java
@@ -1,39 +1,57 @@
 package org.usfirst.frc.team1510.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+// import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.CANTalon;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.GenericHID;
+// import edu.wpi.first.wpilibj.Encoder;
+// import edu.wpi.first.wpilibj.RobotDrive;
+// import edu.wpi.first.wpilibj.GenericHID;
 
 class PIDcontroller {
 
-    private static final double Kp = 0.5, // K constants, gotta tune them
-                                Ki = 0.1,
-                                Kd = -0.1;
-    
-    public static double speedMulti = 1;
+    public double Kp = 0, // K constants, gotta tune them. Kp is multiplier of error. Should be between .5 and 1.5 GENERALLY ---- needs testing
+                  Ki = 0, // Ki is multiplier of the integral. Should be pretty small (less than 1)
+                  Kd = 0; // Kd is multiplier of derivivitve. Should be <0 iirc
+                  //see? it spells pid!
+    public double speedMulti = 1;
 
-    private static double previousError = 0,
-                          previousSpeed = 0;
+    private double previousError = 0,
+                   previousSpeed = 0;
 
-    public static CANTalon encoder;
-    
-    private static double lastEncoderSpeed = 0;
+    public CANTalon encoder;
 
-    // private static double prevError = 0.0; // Used for calculating Ki or something
+    // private  double prevError = 0.0; // Used for calculating Ki or something
 
     private double goalSpeed = 0.0, // For left and right motors, respectively
                    currentSpeed = 0.0;
 
-    private static double returnSpeed = 0.0; // speed to pass to motor controlling class (the direct one, not through PID)
+    private double returnSpeed = 0.0; // speed to pass to motor controlling class (the direct one, not through PID)
 
-    private static double integrator = 0.0, // for left and right motors, respectively
-                          derivator = 0.0;
+    private double integrator = 0.0, // for left and right motors, respectively
+                   derivator = 0.0;
     
     //private static double integratorMax = 0.0, // Max and min values of integrator, to prevent it from being stupid. Depricated, cause i'm using the cool variable thingy. 
     //                      integratorMin = 0.0; 
     
+    public PIDcontroller() {
+    }
+
+    public PIDcontroller(double Kp, double Ki, double Kd) {
+        this.Kp = Kp;
+        this.Ki = Ki;
+        this.Kd = Kd;
+    }
+
+
+    public PIDcontroller(CANTalon encoder) {
+        this.encoder = encoder;
+    }
+
+    public PIDcontroller(double Kp, double Ki, double Kd, CANTalon encoder) {
+        this.Kp = Kp;
+        this.Ki = Ki;
+        this.Kd = Kd;
+        this.encoder = encoder;
+    }
 
     private double CalcError(double speed, double goalspeed) { // Calculates error from input values
         return goalspeed - speed;
@@ -78,7 +96,7 @@ class PIDcontroller {
     }
 
     public double PIDRun() {
-        if (encoder != null) {
+        if (encoder != null && (Kp != 0 ||  Ki != 0 || Kd != 0)) { // making sure encoder and at least one K variable is assigned
             currentSpeed = encoder.getSpeed() * speedMulti;
             this.currentSpeed = currentSpeed;
 
@@ -95,15 +113,9 @@ class PIDcontroller {
             this.previousSpeed = currentSpeed;
             
             this.returnSpeed = pValue + iValue + dValue;
-            return this.returnSpeed;
-        }
 
-        private void SetDrive(
+            return this.returnSpeed; // what will be doing motor driving iirc
 
-        else {
-            return 0;
-        }
-
+        } else return 0;
     }
-    
 }
