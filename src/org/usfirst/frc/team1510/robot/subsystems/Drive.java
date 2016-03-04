7875a2a07ec7dd9839ce49d74d1ac4c5304f6f71
Solomon Greenberg <solomongreenberg@gmail.com>
2016-03-03 17:45:28 -0800

    started to mod drive code to work with my stuff

--- src/org/usfirst/frc/team1510/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team1510/robot/subsystems/Drive.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.GenericHID;
 
+import org.usfirst.frc.team1510.robot.subsystems.PIDcontroller;
 /**
  *
  */
@@ -38,7 +39,6 @@ public class Drive extends Subsystem {
     // Speed ramping variables
     private double[] currentSpeed = {0.0, 0.0};
     private double[] goalSpeed = {0.0, 0.0};
-    private double speedAdjustPerCycle = 0.04;
 
     /**
      * Move based on left and right motor values
@@ -46,6 +46,7 @@ public class Drive extends Subsystem {
      * @param left The left motor value
      * @param right The right motor value
      */
+
     public void move(double left, double right) {
     	if (!enabled) return;
     	//drive.tankDrive(left,right,true);
@@ -53,29 +54,7 @@ public class Drive extends Subsystem {
     	// Set goal speed
     	goalSpeed[0] = left;
     	goalSpeed[1] = right;
-    	
-    	// Logic for left motors
-    	if (Math.abs(goalSpeed[0] - currentSpeed[0]) < speedAdjustPerCycle)
-    		// If within one-cycle range of goal
-    		currentSpeed[0] = goalSpeed[0];
-    	else if (currentSpeed[0] > goalSpeed[0])
-    		// If more than goal
-    		currentSpeed[0] -= speedAdjustPerCycle;
-    	else if (currentSpeed[0] < goalSpeed[0])
-    		// If less than goal
-    		currentSpeed[0] += speedAdjustPerCycle;
-    	
-    	// Logic for right motors
-    	if (Math.abs(goalSpeed[1] - currentSpeed[1]) < speedAdjustPerCycle)
-    		// If within one-cycle range of goal
-    		currentSpeed[1] = goalSpeed[1];
-    	else if (currentSpeed[1] > goalSpeed[1])
-    		// If more than goal
-    		currentSpeed[1] -= speedAdjustPerCycle;
-    	else if (currentSpeed[1] < goalSpeed[1])
-    		// If less than goal
-    		currentSpeed[1] += speedAdjustPerCycle;
-    	
+    
     	// Update motor throttle
     	drive.tankDrive(currentSpeed[0], currentSpeed[1], true);
     }
