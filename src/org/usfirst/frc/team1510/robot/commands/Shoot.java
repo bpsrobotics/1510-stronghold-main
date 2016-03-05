f33d7800bdfe2e9534f404c53f49f01d672f0b40
Stephen Brimhall <stephen.brimhall@icloud.com>
2016-03-04 17:42:52 -0800

    Made shoot work

--- src/org/usfirst/frc/team1510/robot/commands/Shoot.java
+++ src/org/usfirst/frc/team1510/robot/commands/Shoot.java
@@ -3,34 +3,51 @@ package org.usfirst.frc.team1510.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team1510.robot.Robot;
 import org.usfirst.frc.team1510.robot.subsystems.Shooter;
+import org.usfirst.frc.team1510.robot.subsystems.BallCollector;
 import org.usfirst.frc.team1510.robot.subsystems.UltrasonicSubsystem;
 /**
  *
  */
 public class Shoot extends Command {
 	
-	Shooter shooter = Robot.shooter;
-	UltrasonicSubsystem sonic = new UltrasonicSubsystem(1,2);
+    Shooter shooter = Robot.shooter;
+
+    BallCollector pickup = Robot.ballCollector;
+    
+    UltrasonicSubsystem sonic = new UltrasonicSubsystem(1,2);
+
+    private double distance;
+    private int cycles = 0;
+    private boolean complete = false;
+    
     public Shoot() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.shooter);
-    	
+	requires(Robot.ballCollector);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	double distance = sonic.getMillimeters()/1000;
-    	shooter.fire(distance);
+    	distance = sonic.getMillimeters()/1000;
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+	if (cycles > 150) {
+	    pickup.off();
+	    complete = true;
+	}
+	else if (cycles > 100) {
+	    pickup.forward();
+	}
+	shooter.fire(distance);
+	cycles++;
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return complete;
     }
 
     // Called once after isFinished returns true
