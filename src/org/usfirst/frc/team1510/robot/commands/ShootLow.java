4ec26848aa4366bd0d99a90c60d5451dd9c9eb09
annanixon <annapnixon@gmail.com>
2016-03-10 22:49:38 -0800

    Apparently 1 sec waits in a 20 millisec loop are not okay

--- src/org/usfirst/frc/team1510/robot/commands/ShootLow.java
+++ src/org/usfirst/frc/team1510/robot/commands/ShootLow.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1510.robot.commands;
 
 import org.usfirst.frc.team1510.robot.Robot;
+//import org.usfirst.frc.team1510.robot.commands.Teleop.ShooterStage;
 import org.usfirst.frc.team1510.robot.subsystems.BallCollector;
 import org.usfirst.frc.team1510.robot.subsystems.Shooter;
 
@@ -10,7 +11,11 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class ShootLow extends Command {
-
+	private enum ShooterStage {
+		STARTSPIN, SPIN, STARTSHOOT, SHOOT, STOP, OFF
+	}
+	private ShooterStage shooterStage = ShooterStage.OFF;
+	private int timeCounter = 0;
 	Shooter shooter = Robot.shooter;
 	BallCollector ballCollector= Robot.ballCollector;
 	boolean isDone;
@@ -27,6 +32,56 @@ public class ShootLow extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+		switch (shooterStage) {
+		case OFF:
+			shooterStage = ShooterStage.STARTSPIN;
+		case STARTSPIN:
+			// Begin spinning motors and reset time counter
+			shooter.changeDistance(1);
+			shooter.changeHeight(.25);
+			timeCounter = 0;
+			// Advance to next stage
+			shooterStage = ShooterStage.SPIN;
+			// Delay advancement to next runthrough
+			break;
+		case SPIN:
+			// Count down to 0.25 seconds for motor spinup
+			timeCounter += 20;
+			if (timeCounter >= 250) {
+				// Once time has been reached, advance to shoot
+				shooterStage = ShooterStage.STARTSHOOT;
+			} else {
+				// If not time reached, exit switch
+				break;
+			}
+		case STARTSHOOT:
+			// Reset time counter
+			timeCounter = 0;
+			// Turn on collector
+			ballCollector.forward();
+			// Advance to next stage
+			shooterStage = ShooterStage.SHOOT;
+			// Delay advancement to next runthrough
+			break;
+		case SHOOT:
+			// Advance time counter by 0.02 seconds
+			timeCounter += 20;
+			if (timeCounter >= 1000) {
+				// If 1 second reached, advance to stop
+				shooterStage = ShooterStage.STOP;
+			} else {
+				// If not time reached, exit switch
+				break;
+			}
+		case STOP:
+			// Turn off all motors
+			shooter.stop();
+			ballCollector.off();
+			// Advance to next stage
+			shooterStage = ShooterStage.OFF;
+			break;
+		}
+    	/*
     	//Set speed of shooting motors
     	shooter.changeDistance(1);
 		shooter.changeHeight(.25);
@@ -48,6 +103,7 @@ public class ShootLow extends Command {
 		shooter.stop();
 		ballCollector.off();
 		isDone = true;
+		*/
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -64,5 +120,6 @@ public class ShootLow extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
