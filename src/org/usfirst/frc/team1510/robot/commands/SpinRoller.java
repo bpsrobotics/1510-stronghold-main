23ac865c3db95da2642aee0b53685d965c355151
annanixon <annapnixon@gmail.com>
2016-03-14 20:46:26 -0700

    Added new commands

---
+++ src/org/usfirst/frc/team1510/robot/commands/SpinRoller.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team1510.robot.commands;
+
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team1510.robot.Robot;
+import org.usfirst.frc.team1510.robot.subsystems.BallCollector;
+
+/**
+ *
+ */
+public class SpinRoller extends Command {
+	BallCollector ballCollector = Robot.ballCollector;
+	
+    public SpinRoller(int time) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.ballCollector);
+    	setTimeout(time);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	ballCollector.forward();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return isTimedOut();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
