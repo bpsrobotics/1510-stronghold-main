d179909b39e519ff405b5d29325b26a70d0b23f1
annanixon <annapnixon@gmail.com>
2016-04-01 13:32:41 -0700

    Low bar and shoot added to dash

--- src/org/usfirst/frc/team1510/robot/Robot.java
+++ src/org/usfirst/frc/team1510/robot/Robot.java
@@ -86,7 +86,7 @@ public class Robot extends IterativeRobot {
     	defense.addObject("Spy Bot", Autonomous.Defense.SPYBOT);
     	defense.addObject("Move Forward", Autonomous.Defense.ROUGH_TERRAIN);
     	defense.addObject("Low Bar", Autonomous.Defense.LOW_BAR);
-    	defense.addObject("Encoder Move", Autonomous.Defense.ROCK_WALL);
+    	defense.addObject("Low Bar and Shoot", Autonomous.Defense.ROCK_WALL);
     	SmartDashboard.putData("Defense", defense);
 	/*
 	  chooser = new SendableChooser();
